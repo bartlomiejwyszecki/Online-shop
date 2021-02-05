a8323b16a99bf7f8d92871d8b8726b154644a197
bartlomiejwyszecki <bartlomiej.wyszecki2@gmail.com>
2021-02-05 22:56:17 +0100

    Added categories to category list, but still not finding products matched to chosen category.

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,12 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductService } from '../../data/data';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'search',
-  templateUrl: './search.html'
+  templateUrl: './search.html',
+  providers: [ProductService]
 })
 export class SearchComponent { 
-  @Input() count: number = 5; 
-  @Input() rating: number = 0;
+  categories!: string[];
+  formModel: FormGroup;
+
+  constructor(private ProductService: ProductService) {
+    this.categories = this.ProductService.getAllCategories();
+
+    const fb = new FormBuilder();
+    this.formModel = fb.group({
+      'price': [null, positiveNumberValidator],
+      'category': [-1]
+    })
+  }
+
+  onSearch() {
+    if (this.formModel.valid) {
+      console.log(this.formModel.value);
+    }
+  }
+}
+
+function positiveNumberValidator(control: FormControl): any {
+  if (!control.value) return null;
+  const price = parseInt(control.value);
+  return price === null || typeof price === 'number' && price > 0 ?
+    null : { positivenumber: true };
 }
 
 
--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -43,6 +43,10 @@ export class Product {
         .filter(r => r.productId === productId)
         .map(r => new Review(r.id, r.productId, new Date(r.timestamp), r.user, r.rating, r.comment));
     }
+
+    getAllCategories(): string[] {
+      return ['Szosowe', 'Szosowe klasyczne', 'Górskie', 'ABC', 'DEF', 'XYZ'];
+    }
   }
   
   let products = [
@@ -52,7 +56,7 @@ export class Product {
       "price": 2299.99,
       "rating": 4.3,
       "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "categories": ["electronics", "hardware"],
+      "categories": ["Szosowe", "XYZ"],
       "sale": "-10%!"
     },
     {
@@ -61,7 +65,7 @@ export class Product {
       "price": 1699.99,
       "rating": 3.5,
       "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "categories": ["books"]
+      "categories": ["Szosowe klasyczne", "XYZ"]
     },
     {
       "id": 2,
@@ -69,7 +73,7 @@ export class Product {
       "price": 2499.99,
       "rating": 4.2,
       "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "categories": ["electronics"]
+      "categories": ["Szosowe", "ABC"]
     },
     {
       "id": 3,
@@ -77,7 +81,7 @@ export class Product {
       "price": 1559.99,
       "rating": 3.9,
       "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "categories": ["hardware"],
+      "categories": ["Szosowe klasyczne", "ABC"],
       "sale": "-35%!"
     },
     {
@@ -86,7 +90,7 @@ export class Product {
       "price": 2999.99,
       "rating": 5,
       "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "categories": ["electronics", "hardware"]
+      "categories": ["Górskie", "DEF"]
     },
     {
       "id": 5,
@@ -94,7 +98,23 @@ export class Product {
       "price": 2499.99,
       "rating": 4.6,
       "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "categories": ["books"]
+      "categories": ["Szosowe", "DEF"]
+    },
+    {
+      "id": 6,
+      "title": "Siódmy produkt",
+      "price": 3199.99,
+      "rating": 4.1,
+      "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "categories": ["Górskie", "DEF"]
+    },
+    {
+      "id": 7,
+      "title": "Siódmy produkt",
+      "price": 2399.99,
+      "rating": 3.9,
+      "description": "To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "categories": ["Górskie", "XYZ"]
     }
   ];
 
@@ -102,7 +122,7 @@ export class Product {
     {
       "id": 0,
       "productId": 0,
-      "timestamp": "2014-05-20T02:17:00+00:00",
+      "timestamp": "2020-12-20T02:17:00+00:00",
       "user": "Bartłomiej",
       "rating": 5,
       "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
@@ -110,7 +130,7 @@ export class Product {
     {
       "id": 1,
       "productId": 0,
-      "timestamp": "2014-05-20T02:53:00+00:00",
+      "timestamp": "2021-01-20T02:53:00+00:00",
       "user": "Aleksandra",
       "rating": 3,
       "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
@@ -118,7 +138,7 @@ export class Product {
     {
       "id": 2,
       "productId": 5,
-      "timestamp": "2014-05-20T05:26:00+00:00",
+      "timestamp": "2021-01-12T05:26:00+00:00",
       "user": "Paweł",
       "rating": 4,
       "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
@@ -126,7 +146,7 @@ export class Product {
     {
       "id": 3,
       "productId": 2,
-      "timestamp": "2014-05-20T07:20:00+00:00",
+      "timestamp": "2021-01-09T07:20:00+00:00",
       "user": "Wiktoria",
       "rating": 4,
       "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
@@ -134,7 +154,7 @@ export class Product {
     {
       "id": 4,
       "productId": 3,
-      "timestamp": "2014-05-20T11:35:00+00:00",
+      "timestamp": "2021-01-10T11:35:00+00:00",
       "user": "Kajetan",
       "rating": 5,
       "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
@@ -142,9 +162,41 @@ export class Product {
     {
       "id": 5,
       "productId": 2,
-      "timestamp": "2014-05-20T11:42:00+00:00",
+      "timestamp": "2021-01-13T11:42:00+00:00",
       "user": "Justyna",
       "rating": 5,
+      "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
+    },
+    {
+      "id": 6,
+      "productId": 6,
+      "timestamp": "2021-01-07T11:42:00+00:00",
+      "user": "Natalia",
+      "rating": 4,
+      "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
+    },
+    {
+      "id": 7,
+      "productId": 6,
+      "timestamp": "2021-01-14T11:42:00+00:00",
+      "user": "Adrian",
+      "rating": 4,
+      "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
+    },
+    {
+      "id": 8,
+      "productId": 3,
+      "timestamp": "2021-01-29T11:42:00+00:00",
+      "user": "Filip",
+      "rating": 5,
+      "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
+    },
+    {
+      "id": 9,
+      "productId": 7,
+      "timestamp": "2021-02-01T11:42:00+00:00",
+      "user": "Łucja",
+      "rating": 4,
       "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
     }
   ];
